c932c2fcad2e5986a550a4bd0fadf55c35c2e84c
hadisptr <anandahadisaputra@gmail.com>
2023-11-07 18:22:11 +0700

    1

---
+++ dbConnection.php
@@ -0,0 +1,8 @@
+<?php
+$server = "localhost";
+$user = "root";
+$password = "";
+$nama_database = "test";
+
+// Open a new connection to the MySQL server
+$mysqli = mysqli_connect($server, $user, $password, $nama_database);
